0c6c31422872cd2e78bd50922c7d2f473eda28df
Yohann Boniface <edhyjox@gmail.com>
2021-08-13 22:28:17 +0200

    adding debug mode and keyhandling

--- src/__init__.py
+++ src/__init__.py
@@ -4,14 +4,14 @@ import pygame
 
 pygame.init()
 
-
 WIDTH: int = 960
 HEIGHT: int = 720
 SCREEN_SIZE: Tuple[int, int] = (WIDTH, HEIGHT)
 
 FPS_LIMIT: int = 60
+TITLE: str = "Tile Scrolling Platformer"
 
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption('Tile Scrolling Platformer')
-icon = pygame.image.load('assets/icon.png').convert_alpha()
+screen: pygame.display = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption(TITLE)
+icon: pygame.Surface = pygame.image.load("assets/icon.png").convert_alpha()
 pygame.display.set_icon(icon)
--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+from typing import Set
+
 import pygame
 
-from . import FPS_LIMIT
+from . import FPS_LIMIT, TITLE
 
 
 class App:
@@ -9,17 +11,42 @@ class App:
         self.clock: pygame.time.Clock = pygame.time.Clock()
         self.is_running: bool = True
 
+        self.debug = False
+        self.fps_limit = FPS_LIMIT
+
+        self.pressed: Set[int] = set()
+
     def handle_event(self, event: pygame.event.Event) -> None:
         if event.type == pygame.QUIT:
             self.is_running = False
 
+        elif event.type == pygame.KEYDOWN:
+            self.pressed.add(event.key)
+
+            if event.key == pygame.K_f:
+                self.debug = not self.debug
+
+                self.fps_limit = -1 if self.debug else FPS_LIMIT
+
+                if not self.debug:
+                    pygame.display.set_caption(TITLE)
+
+        elif event.type == pygame.KEYUP:
+            self.pressed.remove(event.key)
+
     def run(self) -> None:
         while self.is_running:
+
+            if self.debug:
+                pygame.display.set_caption(
+                    f"{TITLE} - {self.clock.get_fps():.0f} fps"
+                )
+
             for event in pygame.event.get():
                 self.handle_event(event)
 
             pygame.display.update()
-            self.clock.tick(FPS_LIMIT)
+            self.clock.tick(self.fps_limit)
 
     def __del__(self) -> None:
         pygame.display.quit()
