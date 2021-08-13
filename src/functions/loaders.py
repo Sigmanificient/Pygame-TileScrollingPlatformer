2b6823c1dd7e0547f1f78633cc6b0282f299d86a
Yohann Boniface <edhyjox@gmail.com>
2021-08-13 22:47:42 +0200

    adding texture loader

---
+++ src/functions/loaders.py
@@ -0,0 +1,13 @@
+from typing import Dict, Tuple
+
+import pygame
+
+
+def load_sprites_from_dir(
+    directory: str, file_list: Tuple[str]
+) -> Dict[str, pygame.Surface]:
+    """Return a dictionary of the map sprites from a given directory."""
+    return {
+        file_name: pygame.image.load(f'{directory}/{file_name}.png')
+        for file_name in file_list
+    }
